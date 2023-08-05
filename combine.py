a6893ab3c89572bc6675317dff6981646330388a
DollyVadaliya <140232322+DollyVadaliya@users.noreply.github.com>
2023-08-05 11:34:46 +0530

    Add files via upload

---
+++ combine.py
@@ -0,0 +1,62 @@
+"""
+This is a file containing comments, variables,data types,syntax,casting.
+
+LET'S START
+
+"""
+
+#PLEASE ASSUME
+
+a = 20
+b = "DOLLY"
+c = 8j
+d = 8.98
+
+print(a)    #PRINT() 
+print(b)
+print(c)
+print(d)
+
+
+print(type(a))   #TYPE()
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+a = int(5.978)         #TYPE CASTING
+b = str('dolly')
+c = float(89)
+
+d = e = f = 'old'     #ONE VALUE  TO MULTIPLE VARIABLES
+
+print(a)
+print(b)
+print(c)
+
+print(d)
+print(e)
+print(f)
+
+names = ['Arya', 'Dimple', 'Radhika']         #UNPACK VALUES
+d,g,f = names
+print(d)
+print(f)
+print(g)
+
+
+
+
+
+a = int(input('Enter any number '))     #IF ELSE CONDITION 
+if a>=80:
+    print('It is a greater number')
+elif a<50:
+    print('It is center ranged number')
+else:
+    print('It  is a smaller number')
+
+
+
+
+    
